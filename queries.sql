cac4a5fb70f6346229518a641fdb1e977d992cf9
tarantil120 <prosyankin.vladislav@gmail.com>
2022-05-16 11:13:31 +0300

    Добавление данных в БД, SQL-запросы (конечная версия)

--- queries.sql
+++ queries.sql
@@ -12,11 +12,30 @@ VALUES (NULL, '1', '1', '1', '2022-05-16 09:24:08.000000', '2014 Rossignol Distr
        (NULL, '1', '4', '1', '2022-05-16 09:33:35.000000', 'Куртка для сноуборда DC Mutiny Charocal', 'asdasd', 'lot-5.jpg', '7500', '2022-05-17 09:33:35.000000', '100'),
        (NULL, '2', '6', '2', '2022-05-16 09:34:33.000000', 'Маска Oakley Canopy', 'asfqweqwrzxczxczxczczxc', 'lot-6.jpg', '5400', '2022-05-17 09:34:33.000000', '100');
 
-INSERT INTO `steps` (`id_steps`, `id_user`, `date_placement`, `sum`) VALUES (NULL, '1', '2022-05-16 09:36:17.000000', '11099'),   /* добавление ставок для объявления */
-                                                                            (NULL, '2', '2022-05-16 09:41:51.000000', '11199');
+INSERT INTO `steps` (`id_steps`, `id_user`,`id_lot`, `date_placement`, `sum`) VALUES (NULL, '1','1', '2022-05-16 09:36:17.000000', '11099'), /* добавление  в бд данных о ставках пользователей на лоты */
+                                                                                     (NULL, '2','1', '2022-05-16 09:41:51.000000', '11199');
 
 SELECT * FROM `category`;  /* получить все данные о категориях */
 
+SELECT a.Name,
+       start_price,
+       image,
+       (sum(b.sum)+a.start_price)Последняя_ставка,
+       count(b.sum)Количество_ставок,
+       c.name,
+       id_winner
+from lot as a
+       left join steps b
+                 on a.id_lot = b.id_lot
+       inner join category c on a.id_category = c.id_category
+group by a.name, start_price, image,creation_date,date_end, c.name, id_winner
+having ISNULL(date_end)
+order by creation_date desc; /*получить самые новые, открытые лоты. Каждый лот должен включать
+название, стартовую цену, ссылку на изображение, цену последней ставки,
+количество ставок, название категории;*/
+
 SELECT `id_lot`,`category`.`name` FROM lot INNER JOIN category ON `lot`.`id_category` = `category`.`id_category`; /* получить id лота и название категории по id */
+
 UPDATE `lot` SET `name`= 'You my butterfly, sugar, lady' WHERE `id_lot` = 1;   UPDATE `lot` SET `name`= '2014 Rossignol District Snowboard' WHERE `id_lot` = 1;   /* обновить название лота по его идентификатору */
+
 SELECT `id_steps`,`steps`.`id_user`,`date_placement`,`sum` FROM `steps` INNER JOIN `users` ON `steps`.`id_user` = `users`.`id_user` WHERE DATE(`date_placement`) = '2022-05-16';  /* получить список самых свежих ставок для лота по его идентификатору;  */
--- yeticave.sql
+++ yeticave.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 13 2022 г., 11:28
+-- Время создания: Май 16 2022 г., 11:12
 -- Версия сервера: 5.7.33-log
 -- Версия PHP: 7.1.33
 
@@ -32,6 +32,18 @@ CREATE TABLE `category` (
   `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Дамп данных таблицы `category`
+--
+
+INSERT INTO `category` (`id_category`, `name`) VALUES
+(1, 'Доски и лыжи'),
+(2, 'Крепления'),
+(3, 'Ботинки'),
+(4, 'Одежда'),
+(5, 'Инструменты'),
+(6, 'Разное');
+
 -- --------------------------------------------------------
 
 --
@@ -48,9 +60,21 @@ CREATE TABLE `lot` (
   `description` varchar(500) NOT NULL,
   `image` varchar(250) NOT NULL,
   `start_price` int(11) NOT NULL,
-  `date_end` datetime NOT NULL,
+  `date_end` datetime DEFAULT NULL,
   `step_price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `lot`
+--
+
+INSERT INTO `lot` (`id_lot`, `id_user`, `id_category`, `id_winner`, `creation_date`, `name`, `description`, `image`, `start_price`, `date_end`, `step_price`) VALUES
+(1, 1, 1, 1, '2022-05-16 09:24:08', '2014 Rossignol District Snowboard', 'Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив снег мощным щелчкоми четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот снаряд отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом кэмбер позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется, просто посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла равнодушным.', 'lot-1.jpg', 10999, '2022-05-17 09:24:08', 100),
+(2, 2, 1, 2, '2022-05-16 09:27:27', 'DC Ply Mens 2016/2017 Snowboard', 'Легкий маневренный сноуборд, готовый дать жару в любом парке, растопив снег мощным щелчкоми четкими дугами. Стекловолокно Bi-Ax, уложенное в двух направлениях, наделяет этот снаряд отличной гибкостью и отзывчивостью, а симметричная геометрия в сочетании с классическим прогибом кэмбер позволит уверенно держать высокие скорости. А если к концу катального дня сил совсем не останется, просто посмотрите на Вашу доску и улыбнитесь, крутая графика от Шона Кливера еще никого не оставляла равнодушным.', 'lot-2.jpg', 159999, '2022-05-17 09:27:27', 100),
+(3, 1, 2, 1, '2022-05-16 09:30:30', 'Крепления Union Contact Pro 2015 года размер L/XL', 'f', 'lot-3.jpg', 8000, NULL, 100),
+(4, 2, 3, 2, '2022-05-16 09:30:57', 'Ботинки для сноуборда DC Mutiny Charocal', 'a', 'lot-4.jpg', 10999, '2022-05-17 09:30:57', 100),
+(5, 1, 4, 1, '2022-05-16 09:33:35', 'Куртка для сноуборда DC Mutiny Charocal', 'asdasd', 'lot-5.jpg', 7500, '2022-05-17 09:33:35', 100),
+(6, 2, 6, 2, '2022-05-16 09:34:33', 'Маска Oakley Canopy', 'asfqweqwrzxczxczxczczxc', 'lot-6.jpg', 5400, '2022-05-17 09:34:33', 100);
 
 -- --------------------------------------------------------
 
@@ -61,9 +85,18 @@ CREATE TABLE `lot` (
 CREATE TABLE `steps` (
   `id_steps` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
+  `id_lot` int(11) NOT NULL,
   `date_placement` datetime NOT NULL,
   `sum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `steps`
+--
+
+INSERT INTO `steps` (`id_steps`, `id_user`, `id_lot`, `date_placement`, `sum`) VALUES
+(1, 1, 1, '2022-05-16 09:36:17', 11099),
+(2, 2, 1, '2022-05-16 09:41:51', 11199);
 
 -- --------------------------------------------------------
 
@@ -80,6 +113,14 @@ CREATE TABLE `users` (
   `avatar` varchar(250) NOT NULL,
   `contacts` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id_user`, `date_register`, `email`, `name`, `password`, `avatar`, `contacts`) VALUES
+(1, '2022-05-16 09:20:05', 'prosyankin.vlad@yandex.ru', 'Владислав', 'asdafaraasdasd', 'asdasda', 'dasdasdasd'),
+(2, '2022-05-16 09:30:05', 'koksmc2002@gmail.com', 'Дмитрий', 'zzzzzzzzzzz', 'ssssssss', 'qqqqqqqqqqqqq');
 
 --
 -- Индексы сохранённых таблиц
@@ -105,7 +146,8 @@ ALTER TABLE `lot`
 --
 ALTER TABLE `steps`
   ADD PRIMARY KEY (`id_steps`),
-  ADD KEY `id_user` (`id_user`);
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_lot` (`id_lot`);
 
 --
 -- Индексы таблицы `users`
@@ -121,25 +163,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `lot`
 --
 ALTER TABLE `lot`
-  MODIFY `id_lot` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_lot` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT для таблицы `steps`
 --
 ALTER TABLE `steps`
-  MODIFY `id_steps` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_steps` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -157,7 +199,8 @@ ALTER TABLE `lot`
 -- Ограничения внешнего ключа таблицы `steps`
 --
 ALTER TABLE `steps`
-  ADD CONSTRAINT `steps_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `steps_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `steps_ibfk_2` FOREIGN KEY (`id_lot`) REFERENCES `lot` (`id_lot`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
